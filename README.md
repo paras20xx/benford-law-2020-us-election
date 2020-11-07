4943484bbc2689ff25256d5693b0e534d5052e74
Priyank Parashar <paras20xx@gmail.com>
2020-11-08 01:52:33 +0400

    Added graph generation related scripts

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # benford-law-2020-us-election
 Verifying compliance of 2020 US election data against Benford's Law.
+
+# If you wish to generate it on your own
+```sh
+git clone https://github.com/paras20xx/benford-law-2020-us-election.git
+cd benford-law-2020-us-election
+
+# Install Node JS from https://nodejs.org/en/download/
+#     OR
+# Run "nvm install" # https://github.com/nvm-sh/nvm#installing-and-updating
+
+npm install
+npm run copy-files-from-to
+npm run process-dumped-data
+npm start # This would generate graphs from the processed data
+
+# Open the .png files which got generated (These files can also be previewed from README.md)
+```
+
+# Generated graphs
+
+## Milwaukee, Wisconsin
+![Milwaukee](./dump/milwaukee/vote-count/milwaukee-graph.png)
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const seedrandom = require('./utils/seedrandom/seedrandom.js');
+
 let voteCountEntriesForMilwaukee = require('./dump/milwaukee/vote-count/milwaukee.json');
 
 voteCountEntriesForMilwaukee = voteCountEntriesForMilwaukee.map(function (item) {
@@ -17,9 +19,23 @@ for (const voteCountEntryForMilwaukee of voteCountEntriesForMilwaukee) {
         contestants[contestant]++;
     }
 }
+const contestantsArray = Object.keys(contestants);
 
 console.log('\nContestants and the instances of their entries (their count should all be the same):');
 console.log(contestants);
+
+const benfordDistribution = {
+    '_0': 0,
+    '_1': Math.log10(1 + 1) - Math.log10(1),
+    '_2': Math.log10(2 + 1) - Math.log10(2),
+    '_3': Math.log10(3 + 1) - Math.log10(3),
+    '_4': Math.log10(4 + 1) - Math.log10(4),
+    '_5': Math.log10(5 + 1) - Math.log10(5),
+    '_6': Math.log10(6 + 1) - Math.log10(6),
+    '_7': Math.log10(7 + 1) - Math.log10(7),
+    '_8': Math.log10(8 + 1) - Math.log10(8),
+    '_9': Math.log10(9 + 1) - Math.log10(9)
+};
 
 const intialDistribution = {
     '_0': 0,
@@ -51,3 +67,135 @@ for (const contestant in contestants) {
 console.log('\nDistributions:');
 console.log(distributions);
 console.log('');
+
+const { CanvasRenderService } = require('chartjs-node-canvas');
+const fs = require('fs');
+const path = require('path');
+
+const width = 1000;
+const height = 600;
+const chartCallback = (ChartJS) => {
+
+    // Global config example: https://www.chartjs.org/docs/latest/configuration/
+    ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
+    // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
+    ChartJS.plugins.register({
+        // plugin implementation
+    });
+    // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
+    ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
+        // chart implementation
+    });
+};
+const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
+
+// https://github.com/davidbau/seedrandom
+const seededRandom = new seedrandom('this-is-a-seed-string_1');
+
+// https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript/23095818#23095818
+function random_rgba() {
+    var o = Math.round, r = seededRandom, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 1 + ')';
+}
+
+(async () => {
+    const configuration = {
+        type: 'line',
+        data: {
+            labels: [
+                '1',
+                '2',
+                '3',
+                '4',
+                '5',
+                '6',
+                '7',
+                '8',
+                '9'
+            ],
+            datasets: (function () {
+                const output = [];
+
+                const multiplier = voteCountEntriesForMilwaukee.length;
+
+                output.push({
+                    label: 'Ideal',
+                    data: [
+                        multiplier * benfordDistribution['_1'],
+                        multiplier * benfordDistribution['_2'],
+                        multiplier * benfordDistribution['_3'],
+                        multiplier * benfordDistribution['_4'],
+                        multiplier * benfordDistribution['_5'],
+                        multiplier * benfordDistribution['_6'],
+                        multiplier * benfordDistribution['_7'],
+                        multiplier * benfordDistribution['_8'],
+                        multiplier * benfordDistribution['_9']
+                    ],
+                    borderColor: [
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)',
+                        'rgba(255, 255, 255, 1)'
+                    ]
+                });
+
+                for (const contestant of contestantsArray) {
+                    const distributionForContestant = distributions[contestant];
+                    output.push({
+                        label: contestant,
+                        data: [
+                            distributionForContestant['_1'],
+                            distributionForContestant['_2'],
+                            distributionForContestant['_3'],
+                            distributionForContestant['_4'],
+                            distributionForContestant['_5'],
+                            distributionForContestant['_6'],
+                            distributionForContestant['_7'],
+                            distributionForContestant['_8'],
+                            distributionForContestant['_9']
+                        ],
+                        borderColor: [
+                            random_rgba(),
+                            random_rgba(),
+                            random_rgba(),
+                            random_rgba(),
+                            random_rgba(),
+                            random_rgba(),
+                            random_rgba(),
+                            random_rgba(),
+                            random_rgba(),
+                            random_rgba()
+                        ]
+                    });
+                }
+
+                return output;
+            }())
+        }
+    };
+    const image = await canvasRenderService.renderToBuffer(configuration);
+    const dataUrl = await canvasRenderService.renderToDataURL(configuration);
+    const stream = canvasRenderService.renderToStream(configuration);
+
+    const filePath = path.join(
+        __dirname,
+        'dump',
+        'milwaukee',
+        'vote-count',
+        'milwaukee-graph.png'
+    );
+    try {
+        fs.writeFileSync(filePath, image);
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+    console.log(`File written to: ${path.relative(process.cwd(), filePath)}`);
+    console.log('');
+})();
