9db0065e33868a9412775ef969aaefb32ac680f7
Priyank Parashar <paras20xx@gmail.com>
2020-11-09 01:08:37 +0400

    Updated data with various bases to effectively increase the orders of magnitude

--- README.md
+++ README.md
@@ -23,8 +23,67 @@ npm start # This would generate graphs from the processed data
 ## Milwaukee, Wisconsin
 ![Milwaukee](./dump/milwaukee/vote-count/milwaukee-graph.png)
 
+<details>
+  <summary>Milwaukee - Base 3</summary>
+  <p><img alt="Milwaukee - Base 3" src="dump/milwaukee/vote-count/milwaukee-graph-base-3.png"></p>
+</details>
+<details>
+  <summary>Milwaukee - Base 4</summary>
+  <p><img alt="Milwaukee - Base 4" src="dump/milwaukee/vote-count/milwaukee-graph-base-4.png"></p>
+</details>
+<details>
+  <summary>Milwaukee - Base 5</summary>
+  <p><img alt="Milwaukee - Base 5" src="dump/milwaukee/vote-count/milwaukee-graph-base-5.png"></p>
+</details>
+<details>
+  <summary>Milwaukee - Base 6</summary>
+  <p><img alt="Milwaukee - Base 6" src="dump/milwaukee/vote-count/milwaukee-graph-base-6.png"></p>
+</details>
+<details>
+  <summary>Milwaukee - Base 7</summary>
+  <p><img alt="Milwaukee - Base 7" src="dump/milwaukee/vote-count/milwaukee-graph-base-7.png"></p>
+</details>
+<details>
+  <summary>Milwaukee - Base 8</summary>
+  <p><img alt="Milwaukee - Base 8" src="dump/milwaukee/vote-count/milwaukee-graph-base-8.png"></p>
+</details>
+<details>
+  <summary>Milwaukee - Base 9</summary>
+  <p><img alt="Milwaukee - Base 9" src="dump/milwaukee/vote-count/milwaukee-graph-base-9.png"></p>
+</details>
+
 ## Chicago, Illinois
 ![Chicago](./dump/chicago/vote-count/chicago-graph.png)
+
+<details>
+  <summary>Chicago - Base 3</summary>
+  <p><img alt="Chicago - Base 3" src="dump/chicago/vote-count/chicago-graph-base-3.png"></p>
+</details>
+<details>
+  <summary>Chicago - Base 4</summary>
+  <p><img alt="Chicago - Base 4" src="dump/chicago/vote-count/chicago-graph-base-4.png"></p>
+</details>
+<details>
+  <summary>Chicago - Base 5</summary>
+  <p><img alt="Chicago - Base 5" src="dump/chicago/vote-count/chicago-graph-base-5.png"></p>
+</details>
+<details>
+  <summary>Chicago - Base 6</summary>
+  <p><img alt="Chicago - Base 6" src="dump/chicago/vote-count/chicago-graph-base-6.png"></p>
+</details>
+<details>
+  <summary>Chicago - Base 7</summary>
+  <p><img alt="Chicago - Base 7" src="dump/chicago/vote-count/chicago-graph-base-7.png"></p>
+</details>
+<details>
+  <summary>Chicago - Base 8</summary>
+  <p><img alt="Chicago - Base 8" src="dump/chicago/vote-count/chicago-graph-base-8.png"></p>
+</details>
+<details>
+  <summary>Chicago - Base 9</summary>
+  <p><img alt="Chicago - Base 9" src="dump/chicago/vote-count/chicago-graph-base-9.png"></p>
+</details>
+
 
 ## Nebraska
 ![Nebraska](./dump/nebraska/vote-count/nebraska-graph.png)
@@ -40,14 +99,20 @@ npm start # This would generate graphs from the processed data
 ### Georgia - Election day votes
 ![Georgia - Election day votes](./dump/georgia/vote-count/georgia-election-day-votes-graph.png)
 
-### Georgia - Absentee by mail votes
-![Georgia - Absentee by mail votes](./dump/georgia/vote-count/georgia-absentee-by-mail-votes-graph.png)
+<details>
+  <summary>Georgia - Absentee by mail votes</summary>
+  <p><img alt="Georgia - Absentee by mail votes" src="dump/georgia/vote-count/georgia-absentee-by-mail-votes-graph.png"></p>
+</details>
 
-### Georgia - Advanced voting votes
-![Georgia - Advanced voting votes](./dump/georgia/vote-count/georgia-advanced-voting-votes-graph.png)
+<details>
+  <summary>Georgia - Advanced voting votes</summary>
+  <p><img alt="Georgia - Advanced voting votes" src="dump/georgia/vote-count/georgia-advanced-voting-votes-graph.png"></p>
+</details>
 
-### Georgia - Provisional votes
-![Georgia - Provisional votes](./dump/georgia/vote-count/georgia-provisional-votes-graph.png)
+<details>
+  <summary>Georgia - Provisional votes</summary>
+  <p><img alt="Georgia - Provisional votes" src="dump/georgia/vote-count/georgia-provisional-votes-graph.png"></p>
+</details>
 
 ## Washington
 ![Washington](./dump/washington/vote-count/washington-graph.png)
--- index.js
+++ index.js
@@ -60,7 +60,6 @@ const configsForLocations = [
         },
         jsonFilePath: './dump/chicago/vote-count/chicago.json',
         fieldsToDelete: ['field1'],
-        limitCandidates: 3,
         outputGraphPath: path.join(__dirname, 'dump', 'chicago', 'vote-count', 'chicago-graph.png')
     },
 
@@ -141,6 +140,7 @@ const configsForLocations = [
             output: './dump/georgia/vote-count/georgia-election-day-votes.json'
         },
         jsonFilePath: './dump/georgia/vote-count/georgia-election-day-votes.json',
+        limitCandidates: 2,
         outputGraphPath: path.join(__dirname, 'dump', 'georgia', 'vote-count', 'georgia-election-day-votes-graph.png')
     },
     {
@@ -157,6 +157,7 @@ const configsForLocations = [
             output: './dump/georgia/vote-count/georgia-absentee-by-mail-votes.json'
         },
         jsonFilePath: './dump/georgia/vote-count/georgia-absentee-by-mail-votes.json',
+        limitCandidates: 2,
         outputGraphPath: path.join(__dirname, 'dump', 'georgia', 'vote-count', 'georgia-absentee-by-mail-votes-graph.png')
     },
     {
@@ -173,6 +174,7 @@ const configsForLocations = [
             output: './dump/georgia/vote-count/georgia-advanced-voting-votes.json'
         },
         jsonFilePath: './dump/georgia/vote-count/georgia-advanced-voting-votes.json',
+        limitCandidates: 2,
         outputGraphPath: path.join(__dirname, 'dump', 'georgia', 'vote-count', 'georgia-advanced-voting-votes-graph.png')
     },
     {
@@ -189,234 +191,243 @@ const configsForLocations = [
             output: './dump/georgia/vote-count/georgia-provisional-votes.json'
         },
         jsonFilePath: './dump/georgia/vote-count/georgia-provisional-votes.json',
-        limitCandidates: 3,
+        limitCandidates: 2,
         outputGraphPath: path.join(__dirname, 'dump', 'georgia', 'vote-count', 'georgia-provisional-votes-graph.png')
     }
 ];
 
-const base = 10;
+const fromBase = 3;
+const toBase = 10;
 
-for (const configForLocation of configsForLocations) {
-    const limitCandidates = configForLocation.limitCandidates || Infinity;
-    if (configForLocation.mapJson) {
-        let inputJson = require(configForLocation.mapJson.input);
+for (let base = fromBase; base <= toBase; base++) {
+    for (const configForLocation of configsForLocations) {
+        const limitCandidates = configForLocation.limitCandidates || Infinity;
+        if (configForLocation.mapJson) {
+            let inputJson = require(configForLocation.mapJson.input);
 
-        for (const mapper of configForLocation.mapJson.mappers) {
-            if (mapper.filterOutRowsWithoutField) {
-                inputJson = inputJson.filter(function (entry) {
-                    if (entry[mapper.filterOutRowsWithoutField]) {
-                        return true;
-                    }
-                    return false;
-                });
-            }
-            if (mapper.filterInRowsWithNumberLikeData) {
-                inputJson = inputJson.filter(function (entry) {
-                    if (!isNaN(parseInt(entry[mapper.filterInRowsWithNumberLikeData], 10))) {
-                        return true;
-                    }
-                    return false;
-                });
-            }
-            if (mapper.fieldsToDelete) {
-                inputJson = inputJson.map(function (item) {
-                    for (const fieldToDelete of mapper.fieldsToDelete) {
-                        delete item[fieldToDelete];
-                    }
-                    return item;
-                });
-            }
-            if (mapper.renameFields) {
-                inputJson = inputJson.map(function (item) {
-                    for (const renameField of mapper.renameFields) {
-                        item[renameField.to] = item[renameField.from];
-                        delete item[renameField.from];
-                    }
-                    return item;
-                });
-            }
-            if (mapper.converter === 'converter_1') {
-                inputJson = converter_1(inputJson, mapper.options);
-            } else if (mapper.converter === 'converter_2') {
-                inputJson = converter_2(inputJson, mapper.options);
-            } else if (mapper.converter === 'converter_3') {
-                inputJson = converter_3(inputJson, mapper.options);
-            }
-        }
-
-        let str = '';
-
-        str += '[\n';
-        const arr = [];
-        for (const entry of inputJson) {
-            arr.push(JSON.stringify(entry));
-        }
-        str += arr.join(',\n');
-        str += '\n]';
-
-        fs.writeFileSync(
-            path.resolve(__dirname, configForLocation.mapJson.output),
-            str
-        );
-    }
-
-    let voteCountEntriesForLocation = require(configForLocation.jsonFilePath);
-
-    voteCountEntriesForLocation = voteCountEntriesForLocation.map(function (item) {
-        if (configForLocation.fieldsToDelete) {
-            for (const fieldToDelete of configForLocation.fieldsToDelete) {
-                delete item[fieldToDelete];
-            }
-        }
-        return item;
-    });
-
-    const contestants = {};
-    for (const voteCountEntryForLocation of voteCountEntriesForLocation) {
-        for (const contestant in voteCountEntryForLocation) {
-            if (typeof contestants[contestant] === 'undefined') {
-                contestants[contestant] = 0;
-            }
-            contestants[contestant]++;
-        }
-    }
-    let contestantsArray = Object.keys(contestants);
-    if (limitCandidates) {
-        contestantsArray = contestantsArray.slice(0, limitCandidates);
-    }
-
-    console.log('\nContestants and the instances of their entries (their count should all be the same):');
-    console.log(contestants);
-
-    const benfordDistribution = {
-        '_0': 0,
-    };
-    for (let i = 1; i <= base - 1; i++) {
-        benfordDistribution['_' + i] = logForBase(i + 1, base) - logForBase(i, base);
-    }
-
-    const intialDistribution = {
-        '_0': 0,
-    };
-    for (let i = 1; i <= base - 1; i++) {
-        intialDistribution['_' + i] = 0;
-    }
-
-    const distributions = JSON.parse(JSON.stringify(contestants));
-
-    for (const contestant in contestants) {
-        const distributionForContestant = JSON.parse(JSON.stringify(intialDistribution));
-
-        for (const voteCountEntryForLocation of voteCountEntriesForLocation) {
-            const voteCountForContestant = (parseInt(voteCountEntryForLocation[contestant], 10)).toString(base);
-            let firstDigitAsString = '_' + String(voteCountForContestant).substring(0, 1);
-            distributionForContestant[firstDigitAsString]++;
-        }
-
-        distributions[contestant] = distributionForContestant;
-    }
-
-    console.log('\nDistributions:');
-    console.log(distributions);
-    console.log('');
-
-    const width = 1000;
-    const height = 500;
-    const chartCallback = (ChartJS) => {
-
-        // Global config example: https://www.chartjs.org/docs/latest/configuration/
-        ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
-        // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
-        ChartJS.plugins.register({
-            // plugin implementation
-        });
-        // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
-        ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
-            // chart implementation
-        });
-    };
-    const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
-
-    // https://github.com/davidbau/seedrandom
-    const seededRandom = new seedrandom('this-is-a-seed-string_1000');
-
-    // https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript/23095818#23095818
-    function random_rgba() {
-        var o = Math.round, r = seededRandom, s = 255;
-        return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 1 + ')';
-    }
-
-    (async () => {
-        const configuration = {
-            type: 'line',
-            data: {
-                labels: (function () {
-                    const arr = [];
-                    for (let i = 1; i <= base - 1; i++) {
-                        arr.push('' + i);
-                    }
-                    return arr;
-                }()),
-                datasets: (function () {
-                    const output = [];
-
-                    const multiplier = voteCountEntriesForLocation.length;
-
-                    output.push({
-                        label: 'Ideal',
-                        data: (function () {
-                            const arr = [];
-                            for (let i = 1; i <= base - 1; i++) {
-                                arr.push(multiplier * benfordDistribution['_' + i]);
-                            }
-                            return arr;
-                        }()),
-                        borderColor: (function () {
-                            const arr = [];
-                            for (let i = 1; i <= base - 1; i++) {
-                                arr.push('rgba(255, 0, 0, 1)');
-                            }
-                            return arr;
-                        }())
+            for (const mapper of configForLocation.mapJson.mappers) {
+                if (mapper.filterOutRowsWithoutField) {
+                    inputJson = inputJson.filter(function (entry) {
+                        if (entry[mapper.filterOutRowsWithoutField]) {
+                            return true;
+                        }
+                        return false;
                     });
+                }
+                if (mapper.filterInRowsWithNumberLikeData) {
+                    inputJson = inputJson.filter(function (entry) {
+                        if (!isNaN(parseInt(entry[mapper.filterInRowsWithNumberLikeData], 10))) {
+                            return true;
+                        }
+                        return false;
+                    });
+                }
+                if (mapper.fieldsToDelete) {
+                    inputJson = inputJson.map(function (item) {
+                        for (const fieldToDelete of mapper.fieldsToDelete) {
+                            delete item[fieldToDelete];
+                        }
+                        return item;
+                    });
+                }
+                if (mapper.renameFields) {
+                    inputJson = inputJson.map(function (item) {
+                        for (const renameField of mapper.renameFields) {
+                            item[renameField.to] = item[renameField.from];
+                            delete item[renameField.from];
+                        }
+                        return item;
+                    });
+                }
+                if (mapper.converter === 'converter_1') {
+                    inputJson = converter_1(inputJson, mapper.options);
+                } else if (mapper.converter === 'converter_2') {
+                    inputJson = converter_2(inputJson, mapper.options);
+                } else if (mapper.converter === 'converter_3') {
+                    inputJson = converter_3(inputJson, mapper.options);
+                }
+            }
 
-                    for (const contestant of contestantsArray) {
-                        const distributionForContestant = distributions[contestant];
+            let str = '';
+
+            str += '[\n';
+            const arr = [];
+            for (const entry of inputJson) {
+                arr.push(JSON.stringify(entry));
+            }
+            str += arr.join(',\n');
+            str += '\n]';
+
+            // TODO: Move JSON computations and file write operation outside the for loop (and remove the following if condition).
+            if (base === fromBase) {
+                fs.writeFileSync(
+                    path.resolve(__dirname, configForLocation.mapJson.output),
+                    str
+                );
+            }
+        }
+
+        let voteCountEntriesForLocation = require(configForLocation.jsonFilePath);
+
+        voteCountEntriesForLocation = voteCountEntriesForLocation.map(function (item) {
+            if (configForLocation.fieldsToDelete) {
+                for (const fieldToDelete of configForLocation.fieldsToDelete) {
+                    delete item[fieldToDelete];
+                }
+            }
+            return item;
+        });
+
+        const contestants = {};
+        for (const voteCountEntryForLocation of voteCountEntriesForLocation) {
+            for (const contestant in voteCountEntryForLocation) {
+                if (typeof contestants[contestant] === 'undefined') {
+                    contestants[contestant] = 0;
+                }
+                contestants[contestant]++;
+            }
+        }
+        let contestantsArray = Object.keys(contestants);
+        if (limitCandidates) {
+            contestantsArray = contestantsArray.slice(0, limitCandidates);
+        }
+
+        console.log('\nContestants and the instances of their entries (their count should all be the same):');
+        console.log(contestants);
+
+        const benfordDistribution = {
+            '_0': 0,
+        };
+        for (let i = 1; i <= base - 1; i++) {
+            benfordDistribution['_' + i] = logForBase(i + 1, base) - logForBase(i, base);
+        }
+
+        const intialDistribution = {
+            '_0': 0,
+        };
+        for (let i = 1; i <= base - 1; i++) {
+            intialDistribution['_' + i] = 0;
+        }
+
+        const distributions = JSON.parse(JSON.stringify(contestants));
+
+        for (const contestant in contestants) {
+            const distributionForContestant = JSON.parse(JSON.stringify(intialDistribution));
+
+            for (const voteCountEntryForLocation of voteCountEntriesForLocation) {
+                const voteCountForContestant = (parseInt(voteCountEntryForLocation[contestant], 10)).toString(base);
+                let firstDigitAsString = '_' + String(voteCountForContestant).substring(0, 1);
+                distributionForContestant[firstDigitAsString]++;
+            }
+
+            distributions[contestant] = distributionForContestant;
+        }
+
+        console.log('\nDistributions:');
+        console.log(distributions);
+        console.log('');
+
+        const width = 1000;
+        const height = 500;
+        const chartCallback = (ChartJS) => {
+
+            // Global config example: https://www.chartjs.org/docs/latest/configuration/
+            ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
+            // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
+            ChartJS.plugins.register({
+                // plugin implementation
+            });
+            // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
+            ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
+                // chart implementation
+            });
+        };
+        const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
+
+        // https://github.com/davidbau/seedrandom
+        const seededRandom = new seedrandom('this-is-a-seed-string_1000');
+
+        // https://stackoverflow.com/questions/23095637/how-do-you-get-random-rgb-in-javascript/23095818#23095818
+        function random_rgba() {
+            var o = Math.round, r = seededRandom, s = 255;
+            return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 1 + ')';
+        }
+
+        (async () => {
+            const configuration = {
+                type: 'line',
+                data: {
+                    labels: (function () {
+                        const arr = [];
+                        for (let i = 1; i <= base - 1; i++) {
+                            arr.push('' + i);
+                        }
+                        return arr;
+                    }()),
+                    datasets: (function () {
+                        const output = [];
+
+                        const multiplier = voteCountEntriesForLocation.length;
+
                         output.push({
-                            label: contestant,
+                            label: 'Ideal',
                             data: (function () {
                                 const arr = [];
                                 for (let i = 1; i <= base - 1; i++) {
-                                    arr.push(distributionForContestant['_' + i]);
+                                    arr.push(multiplier * benfordDistribution['_' + i]);
                                 }
                                 return arr;
                             }()),
                             borderColor: (function () {
                                 const arr = [];
                                 for (let i = 1; i <= base - 1; i++) {
-                                    arr.push(random_rgba());
+                                    arr.push('rgba(255, 0, 0, 1)');
                                 }
                                 return arr;
                             }())
                         });
-                    }
 
-                    return output;
-                }())
+                        for (const contestant of contestantsArray) {
+                            const distributionForContestant = distributions[contestant];
+                            output.push({
+                                label: contestant,
+                                data: (function () {
+                                    const arr = [];
+                                    for (let i = 1; i <= base - 1; i++) {
+                                        arr.push(distributionForContestant['_' + i]);
+                                    }
+                                    return arr;
+                                }()),
+                                borderColor: (function () {
+                                    const arr = [];
+                                    for (let i = 1; i <= base - 1; i++) {
+                                        arr.push(random_rgba());
+                                    }
+                                    return arr;
+                                }())
+                            });
+                        }
+
+                        return output;
+                    }())
+                }
+            };
+            const image = await canvasRenderService.renderToBuffer(configuration);
+            const dataUrl = await canvasRenderService.renderToDataURL(configuration);
+            const stream = canvasRenderService.renderToStream(configuration);
+
+            let filePath = configForLocation.outputGraphPath;
+            if (base !== 10) {
+                filePath = filePath.replace(/\.png$/, `-base-${base}.png`);
             }
-        };
-        const image = await canvasRenderService.renderToBuffer(configuration);
-        const dataUrl = await canvasRenderService.renderToDataURL(configuration);
-        const stream = canvasRenderService.renderToStream(configuration);
-
-        const filePath = configForLocation.outputGraphPath;
-        try {
-            fs.writeFileSync(filePath, image);
-        } catch (e) {
-            console.log(e);
-            throw e;
-        }
-        console.log(`File written to: ${path.relative(process.cwd(), filePath)}`);
-        console.log('');
-    })();
+            try {
+                fs.writeFileSync(filePath, image);
+            } catch (e) {
+                console.log(e);
+                throw e;
+            }
+            console.log(`File written to: ${path.relative(process.cwd(), filePath)}`);
+            console.log('');
+        })();
+    }
 }
